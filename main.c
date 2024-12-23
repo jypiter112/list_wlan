723b9a020342ca6df06184b2e7ee116356f5fa39
jypiter112 <146531209+jypiter112@users.noreply.github.com>
2024-12-23 21:45:03 +0200

    Cleaned code

--- main.c
+++ main.c
@@ -34,6 +34,42 @@ void print_interface_state(WLAN_INTERFACE_STATE isState){
         break;
     }
 }
+void print_network_info(PWLAN_AVAILABLE_NETWORK pNetworkEntry, unsigned int j){
+    printf("\n\nProfile name [%d]: %s\n", j, wchar_to_cstring(pNetworkEntry->strProfileName));
+    printf("Network type [%d] %lu\n", j, pNetworkEntry->dot11BssType);
+    printf("Connectable [%d] ", j);
+
+    if(pNetworkEntry->bNetworkConnectable){
+        printf("Yes\n");
+    } else {
+        printf("No\n");
+        printf("Reason: %u\n", pNetworkEntry->wlanNotConnectableReason);
+    }
+
+    printf("Connection quality: [%d] %u\n", j, pNetworkEntry->wlanSignalQuality);
+    
+    printf("Security enabled [%d] ", j);
+    if(pNetworkEntry->bSecurityEnabled){
+        printf("Yes\n");
+    } else {
+        printf("No\n");
+    }
+
+    printf("Flags [%d] 0x%lx\n", j, pNetworkEntry->dwFlags);
+    if(pNetworkEntry->dwFlags == 0x03){
+        printf("Currently connected [%d]\n", j);
+    }
+}
+void print_interface_info(PWLAN_INTERFACE_INFO pInterfaceInfo, int* pinterfaceRet, WCHAR *GuidString, unsigned int i){
+    printf("Interface Index: %d\n", i);
+    *pinterfaceRet = StringFromGUID2(&pInterfaceInfo->InterfaceGuid, (LPOLESTR)GuidString,(sizeof(GuidString)/sizeof(*GuidString)));
+    if(pinterfaceRet != 0){
+        printf("IntefaceGUID [%d] %s\n", i, wchar_to_cstring(GuidString));
+    }
+    printf("Interface description: [%d] %s\n", i, wchar_to_cstring(pInterfaceInfo->strInterfaceDescription));
+    printf("Interface state[%d] ", i);
+    print_interface_state(pInterfaceInfo->isState);
+}
 int main(void){
     DWORD lastError;
     DWORD version = 0;
@@ -47,13 +83,14 @@ int main(void){
 
     WCHAR GuidString[39] = {0};
     int interfaceRet = 0;
-
+    
+    // Open handle to WLAN
     lastError = WlanOpenHandle(1, 0, &version, &client);
     if(lastError != ERROR_SUCCESS){
         printf("Error wlanh failed code: 0x%lx", lastError);
         return 1;
     }
-
+    
     lastError = WlanEnumInterfaces(client, 0, &ppInterfaceList);
     if(lastError != ERROR_SUCCESS){
         printf("Error getenuminterfaces failed code: 0x%lx", lastError);
@@ -63,14 +100,7 @@ int main(void){
         for(unsigned int i = 0; i < (int)ppInterfaceList->dwNumberOfItems; i++){
             // Get Current index of interface info
             pInterfaceInfo = (PWLAN_INTERFACE_INFO) &ppInterfaceList->InterfaceInfo[i];
-            printf("Interface Index: %d\n", i);
-            interfaceRet = StringFromGUID2(&pInterfaceInfo->InterfaceGuid, (LPOLESTR)&GuidString,(sizeof(GuidString)/sizeof(*GuidString)));
-            if(interfaceRet != 0){
-                printf("IntefaceGUID [%d] %s\n", i, wchar_to_cstring(GuidString));
-            }
-            printf("Interface description: [%d] %s\n", i, wchar_to_cstring(pInterfaceInfo->strInterfaceDescription));
-            printf("Interface state[%d] ", i);
-            print_interface_state(pInterfaceInfo->isState);
+            print_interface_info(pInterfaceInfo, &interfaceRet, &GuidString, i);
 
             lastError = WlanGetAvailableNetworkList(client, &pInterfaceInfo->InterfaceGuid, 0, NULL, &ppNetworkList);
             if(lastError != ERROR_SUCCESS){
@@ -82,30 +112,7 @@ int main(void){
                 printf("Num of entries [%d]\n\n", ppNetworkList->dwNumberOfItems);
                 for(unsigned int j = 0; j < ppNetworkList->dwNumberOfItems; j++){
                     pNetworkEntry = (WLAN_AVAILABLE_NETWORK *)&ppNetworkList->Network[j];
-                    printf("\n\nProfile name [%d]: %s\n", j, wchar_to_cstring(pNetworkEntry->strProfileName));
-                    printf("Network type [%d] %lu\n", j, pNetworkEntry->dot11BssType);
-                    printf("Connectable [%d] ", j);
-
-                    if(pNetworkEntry->bNetworkConnectable){
-                        printf("Yes\n");
-                    } else {
-                        printf("No\n");
-                        printf("Reason: %u\n", pNetworkEntry->wlanNotConnectableReason);
-                    }
-
-                    printf("Connection quality: [%d] %u\n", j, pNetworkEntry->wlanSignalQuality);
-                    
-                    printf("Security enabled [%d] ", j);
-                    if(pNetworkEntry->bSecurityEnabled){
-                        printf("Yes\n");
-                    } else {
-                        printf("No\n");
-                    }
-
-                    printf("Flags [%d] 0x%lx\n", j, pNetworkEntry->dwFlags);
-                    if(pNetworkEntry->dwFlags == 0x03){
-                        printf("Currently connected [%d]\n", j);
-                    }
+                    print_network_info(pNetworkEntry, j);
                 }
             }
         }
