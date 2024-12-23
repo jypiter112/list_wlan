028c40a4b520b2d3023d41eff76b354d32520996
jypiter112 <146531209+jypiter112@users.noreply.github.com>
2024-12-23 21:55:19 +0200

    Fixed naming

--- main.c
+++ main.c
@@ -8,13 +8,18 @@
 
 CHAR* wchar_to_cstring(const WCHAR* wstr)
 {
-    int wstr_len = (int) wcslen(wstr);
+    if(!wstr){
+        return NULL;
+    }
+    int wstr_len = (int) wcslen(wstr) + 1;
     int num_chars = WideCharToMultiByte(CP_UTF8, 0, wstr, wstr_len, NULL, 0, NULL, NULL);
     CHAR* strTo = (CHAR*) malloc((num_chars + 1) * sizeof(CHAR));
     if (strTo)
     {
         WideCharToMultiByte(CP_UTF8, 0, wstr, wstr_len, strTo, num_chars, NULL, NULL);
         strTo[num_chars] = '\0';
+    } else{
+        free(strTo);
     }
     return strTo;
 }
@@ -35,18 +40,18 @@ void print_interface_state(WLAN_INTERFACE_STATE isState){
     }
 }
 void print_network_info(PWLAN_AVAILABLE_NETWORK pNetworkEntry, unsigned int j){
-    printf("\n\nProfile name [%d]: %s\n", j, wchar_to_cstring(pNetworkEntry->strProfileName));
-    printf("Network type [%d] %lu\n", j, pNetworkEntry->dot11BssType);
+    wprintf(L"\n\nNetwork name [%d]: %s\n", j, pNetworkEntry->strProfileName);
+    printf("Network type [%d] %d\n", j, pNetworkEntry->dot11BssType);
     printf("Connectable [%d] ", j);
 
     if(pNetworkEntry->bNetworkConnectable){
         printf("Yes\n");
     } else {
         printf("No\n");
-        printf("Reason: %u\n", pNetworkEntry->wlanNotConnectableReason);
+        printf("Reason: %lu\n", pNetworkEntry->wlanNotConnectableReason);
     }
 
-    printf("Connection quality: [%d] %u\n", j, pNetworkEntry->wlanSignalQuality);
+    printf("Connection quality: [%d] %lu\n", j, pNetworkEntry->wlanSignalQuality);
     
     printf("Security enabled [%d] ", j);
     if(pNetworkEntry->bSecurityEnabled){
@@ -83,7 +88,7 @@ int main(void){
 
     WCHAR GuidString[39] = {0};
     int interfaceRet = 0;
-    
+
     // Open handle to WLAN
     lastError = WlanOpenHandle(1, 0, &version, &client);
     if(lastError != ERROR_SUCCESS){
@@ -109,7 +114,7 @@ int main(void){
             else {
                 printf("\n\n");
                 printf("Available network list for this interface\n");
-                printf("Num of entries [%d]\n\n", ppNetworkList->dwNumberOfItems);
+                printf("Num of entries %lu\n\n", ppNetworkList->dwNumberOfItems);
                 for(unsigned int j = 0; j < ppNetworkList->dwNumberOfItems; j++){
                     pNetworkEntry = (WLAN_AVAILABLE_NETWORK *)&ppNetworkList->Network[j];
                     print_network_info(pNetworkEntry, j);
